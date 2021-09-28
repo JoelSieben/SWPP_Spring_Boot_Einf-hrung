7a4d5a4440c2e48be0bc954b9e83e1822a6eb865
Joel Sieben <joel.sieben@gmail.com>
2021-09-28 10:32:50 +0200

    Tutorial Part 1 beendet |#1

---
+++ src/main/java/com/example/testuumgebung/TestuumgebungApplication.java
@@ -0,0 +1,13 @@
+package com.example.testuumgebung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestuumgebungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestuumgebungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testuumgebung/TestuumgebungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testuumgebung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestuumgebungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
